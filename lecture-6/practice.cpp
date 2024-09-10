b9dda1593a9379c047419857850af62a5ee46fa9
azimiHamid <azimihamid2003@gmail.com>
2024-09-10 19:16:37 +0500

    Add some functions to the practice set.

--- lecture-6/practice.cpp
+++ lecture-6/practice.cpp
@@ -27,40 +27,87 @@ int binToDecimal(int n) {
     return res;
 }
 
+bool isValidBinary(int n) {
+    while (n > 0) {
+        if (n % 10 > 1) {
+            return false;
+        }
+        n /= 10;
+    }
+    return true;
+}
+
+char getUserChoice() {
+    char choice;
+    bool validInput = false;
+
+    while (!validInput == true) {
+        cout << "\nWhat do you want to do? (Please select A or B)\n";
+        cout << "A) Change a Decimal number to Binary\n";
+        cout << "B) Change a Binary number to Decimal\n";
+        cin >> choice;
+
+        if (choice == 'A' || choice == 'B' || choice == 'a' || choice == 'b') {
+            validInput = true;
+        } else {
+            cout << "\n-------------------------\nInvalid input! Please enter A or B.\n-------------------------\n";
+            cin.clear(); // Clear the error flag set by invalid input
+            cin.ignore(1000, '\n'); // Ignore the rest of the invalid input
+        }
+    }
+    return choice;
+}
+
+bool shouldContinue() {
+    char ch;
+    cout << "Press [Y] to continue or [N] to quite : ";
+    cin >> ch;
+    // return !(ch == 'N' || ch == 'n');  // !(true) = false
+    if (ch == 'N' || ch == 'n') {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+void handleDecToBin() {
+    int num;
+    cout << "Please enter a Decimal number: ";
+    cin >> num;
+    if (num < 0) {
+        cout << "Invalid input! Decimal numbers must be non-negative.\n";
+    } else {
+        cout << "Binary of " << num << " = " << decToBin(num) << endl;
+    }
+}
+
+void handleBinToDec() {
+    int num;
+    cout << "Please enter a Binary number (includes 0s & 1s only): ";
+    cin >> num;
+    if (isValidBinary(num)) {
+        cout << "Decimal of " << num << " = " << binToDecimal(num) << endl;
+    } else {
+        cout << "Invalid input! Binary numbers must contain only 0s and 1s.\n";
+    }
+}
+
 int main() {
 
     bool goAgain = true;
 
     while (goAgain) {
-        char userChoice;
-        cout << "What do you want to do? (Please select A or B)\n";
-        cout << "A) Change a Decimal number to Binary\n";
-        cout << "B) Change a Binary number to Decimal\n";
-        cin >> userChoice;
+        char userChoice = getUserChoice(); 
 
         double num;
         if (userChoice == 'A' || userChoice == 'a') {
-            cout << "Please enter a Decimal number : ";
-            cin >> num;
-            cout << "Binary of " << num << " = " << decToBin(num) << endl;
-            goAgain = false;
+            handleDecToBin();
 
         } else if (userChoice == 'B' || userChoice == 'b') {
-            cout << "Please enter a Binary number (includes 0s & 1s only) : ";
-            cin >> num;
-            cout << "Decimal of " << num << " = " << binToDecimal(num) << endl;
-            goAgain = false;
-
-        } else {
-            cout << "\n-------------------------\nInvalid input! Try again.\n-------------------------\n";
+            handleBinToDec();
         }
 
-        char ch;
-        cout << "Press [Y] to continue or [N] to quite : ";
-        cin >> ch;
-        if (ch == 'N' || ch == 'n') {
-            goAgain = false;
-        }
+        goAgain = shouldContinue();
 
     }
 
