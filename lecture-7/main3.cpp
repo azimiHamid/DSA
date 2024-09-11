455e84f3e2803ee5bf7303ed643fcf38f7894707
azimiHamid <azimihamid2003@gmail.com>
2024-09-11 18:20:09 +0500

    updates readme and files - add scope

--- lecture-7/main3.cpp
+++ lecture-7/main3.cpp
@@ -1,11 +1,32 @@
-// Scope
+// Scope: Scope is the area in a program where a variable is accessible. It determines where the variable can be used, whether inside a function (local scope) or throughout the entire program (global scope).
+// Local scope: Variables declared within a function or block can only be accessed within that specific function or block.
+// Global scope: Variables declared outside of all functions or blocks can be accessed from anywhere in the program.
 
+
+// Scope
 #include <iostream>
 using namespace std;
 
+int fun() {
+    int price = 99; // price --> local scope
+}
+
 int main() {
 
-    cout << 5 - 2 * 6 << endl;    // -7
+    int globalScopeVar = 786;
+
+    fun();
+
+    if (true) {
+        int age = 21; // age --> local scope
+    }
+
+    for (int i = 1; i < 10; i++) {} // i --> Local scope
+
+    cout << i << endl; // error ❌
+    cout << age << endl; // error ❌
+    cout << price << endl; // error ❌
+    cout << globalScopeVar << endl; // 786 ✔️
     
     return 0;
 }
