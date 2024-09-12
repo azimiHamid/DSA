939d38a03bfbd5e53d5c2f81643ce0c91bed0f38
azimiHamid <azimihamid2003@gmail.com>
2024-09-12 13:50:34 +0500

    update assignment 1

--- hw1.cpp
+++ hw1.cpp
@@ -8,24 +8,20 @@ int main() {
     int size = 6;
     int nums[size] = {23, 5, 24, 1, -15, 32};
 
-    int smallest = INT_MAX; // INT_MAX = +infinity
-    int largest = INT_MIN; // INT_MIN = -infinity
     int smallestIdx = 0;
     int largestIdx = 0;
 
-    for (int i = 0; i < size; i++) {
-        smallest = min(nums[i], smallest);
-        largest = max(nums[i], largest);
-        if (nums[i] == smallest) {
+    for (int i = 1; i < size; i++) {
+        if (nums[i] < nums[smallestIdx]) {
             smallestIdx = i;
         }
-        if (nums[i] == largest) {
+        if (nums[i] > nums[largestIdx]) {
             largestIdx = i;
         }
     }
 
-    cout << "index of smallest num = " << smallestIdx << ": " << smallest << endl;
-    cout << "index of largest num = " << largestIdx << ": " << largest << endl;
+    cout << "Index of smallest num = " << smallestIdx << ": " << nums[smallestIdx] << endl;
+    cout << "Index of largest num = " << largestIdx << ": " << nums[largestIdx] << endl;
 
     return 0;
 }
