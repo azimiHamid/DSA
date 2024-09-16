fc4859260a550c01356e8c9dae09df459b883784
azimiHamid <azimihamid2003@gmail.com>
2024-09-16 23:25:50 +0500

    complete chapter 9

--- lecture-9/hw1.cpp
+++ lecture-9/hw1.cpp
@@ -1,17 +1,39 @@
-// Linear search on a vector?
+// A function which reverse a vector - pass by reference?
 
 #include <iostream>
 #include <vector>
 using namespace std;
 
-int singleNumber(vector<int> &nums) {
-    
+void reverseVector(vector<int> &nums, int sz) {
+    int start = 0;
+    int end = sz-1;
+
+    while (start < end) {
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+
+        start++;
+        end--;
+    }
 }
 
 int main() {
 
-    vector<int> nums = {4, 1, 2, 1, 2};
+    vector<int> nums = {1, 2, 3, 4, 5};
+    int size = nums.size();
     
+    cout << "Original Vector:" << endl;
+    for (int num: nums) {
+        cout << num << " ";
+    }
+
+    reverseVector(nums, size);
+
+    cout << "\nReversed Vector:" << endl;
+    for (int num: nums) {
+        cout << num << " ";
+    }
     
     return 0;
 }
--- lecture-9/hw2.cpp
+++ lecture-9/hw2.cpp
@@ -1,17 +1,31 @@
-// A function which reverse a vector - pass by reference?
+// Linear search on a vector?
 
 #include <iostream>
 #include <vector>
 using namespace std;
 
-int singleNumber(vector<int> &nums) {
-    
+int linearSearch(const vector<int>& nums, int target) {
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] == target) {
+            return i;
+        }
+    }
+
+    return -1;
 }
 
 int main() {
 
-    vector<int> nums = {4, 1, 2, 1, 2};
+    vector<int> nums = {10, 20, 30, 40, 50};
+    int target = 30;
     
+    int res = linearSearch(nums, target);
+
+    if (res != -1) {
+        cout << "Number found at index: " << res << endl;
+    } else {
+        cout << "Number not found\n";
+    }
     
     return 0;
 }
