7f57cfcee88e6c7b02729ca2f60c673eb9f892a0
azimiHamid <azimihamid2003@gmail.com>
2024-10-12 10:29:57 +0500

    updates

--- lecture-16/main2.cpp
+++ lecture-16/main2.cpp
@@ -1,23 +1,29 @@
 // POINTERS in C++
-// Special variables that store address of other varibales.
+// Pointers are special variables that store the memory address of another variable.
 
-//  Pointer to Pointer
-//   Store the address of another pointer.
+// Pointer to Pointer
+// A pointer that stores the address of another pointer, creating multiple levels of indirection.
 
 #include <iostream>
 using namespace std;
 
 int main() {
-    int age = 78;
-    int* pointer = &age;  // pointer
+    int age = 78;            // Regular integer variable
+    int* pointer = &age;      // Pointer storing the address of 'age'
 
-    int** pointer2 = &pointer;
+    int** pointer2 = &pointer; // Pointer to pointer, storing the address of 'pointer'
 
-    cout << pointer << endl;
-    cout << &age << endl;
+    // Output the value of 'pointer' (i.e., the address of 'age')
+    cout << "Address stored in pointer (address of 'age'): " << pointer << endl;
+    
+    // Output the address of 'age' directly (should match the value of 'pointer')
+    cout << "Direct address of 'age': " << &age << endl;
 
-    cout << &pointer << endl; // memory address of pointer
-    cout << pointer2 << endl; // pointer2
+    // Output the address of 'pointer' (memory address where 'pointer' is stored)
+    cout << "Address of 'pointer': " << &pointer << endl;
+
+    // Output the value of 'pointer2' (i.e., the address of 'pointer')
+    cout << "Address stored in pointer2 (address of 'pointer'): " << pointer2 << endl;
 
     return 0;
 }
