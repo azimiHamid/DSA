206a9610c9f3449080015a6e41bf155e63421f1f
azimiHamid <azimihamid2003@gmail.com>
2024-10-19 23:13:03 +0500

    updates

--- lecture-20/main.cpp
+++ lecture-20/main.cpp
@@ -71,6 +71,8 @@ int singleNonDuplicate(vector<int>& nums) {
             }
         }
     }
+
+    return -1;
 }
 
 
@@ -79,6 +81,7 @@ int main() {
     vector<int> nums = {3, 3, 7, 7, 10, 11, 11};
     // vector<int> nums = {1, 1, 2, 3, 3, 7, 7, 11, 11};
     // vector<int> nums = {1, 1, 2, 2, 3, 3, 7, 11, 11};
+    cout << "Single Element: " << singleNonDuplicate(nums) << endl;
 
     return 0;
 }
