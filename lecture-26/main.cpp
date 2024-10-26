1053b6686754b5bf33220088ecd64030577a9a80
azimiHamid <azimihamid2003@gmail.com>
2024-10-26 14:07:04 +0500

    update "Merge 2 Sorted Arrays Problem"

--- lecture-26/main.cpp
+++ lecture-26/main.cpp
@@ -4,10 +4,7 @@
 #include <vector>
 using namespace std;
 
-void mergeArrays(vector<int> &arr1, vector<int> &arr2) {
-    int m = 3; // Number of actual elements in arr1
-    int n = arr2.size();
-
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
     // Index pointers for arr1, arr2, and merged array from the back
     int i = m - 1; // 2
     int j = n - 1; // 2
@@ -15,13 +12,14 @@ void mergeArrays(vector<int> &arr1, vector<int> &arr2) {
 
     // Merge arr2 into arr1 from the end
     while (j >= 0) {
-        if (i >= 0 && arr1[i] > arr2[j]) {
-            arr1[k] = arr1[i];
+        if (i >= 0 && nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
             k--; i--;
         } else {
-            arr1[k] = arr2[j]; 
+            nums1[k] = nums2[j]; 
             k--; j--;
         }
+    
     }
 }
 
@@ -29,7 +27,10 @@ int main() {
 
     vector<int> arr1 = {1,2,3,0,0,0};
     vector<int> arr2 = {2,5,6};
-    mergeArrays(arr1, arr2);
+    int m = 3; // Number of actual elements in arr1
+    int n = arr2.size(); // Number of elements in arr2
+
+    merge(arr1, m, arr2, n);
 
     for (int num : arr1) {
         cout << num << " ";
