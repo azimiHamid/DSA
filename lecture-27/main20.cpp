dcbced38cf6cc478645f4291b418261cbbdad954
azimiHamid <azimihamid2003@gmail.com>
2024-10-27 18:02:53 +0500

    Add max and min funcs from Algorithm STL in C++

---
+++ lecture-27/main20.cpp
@@ -0,0 +1,13 @@
+// Other Algorithms : min and max
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    
+    cout << "Maximum of (40, 99) is = " << max(40, 99) << endl;
+    cout << "Minimum of (64, 203) is = " << min(64, 203) << endl;
+
+    return 0;
+}
