6a841b0ffb05bbfc27bffa7fbaeb6a22291d91df
azimiHamid <azimihamid2003@gmail.com>
2024-10-28 15:18:35 +0500

    Add Valid Palindrome leetcode #125

---
+++ lecture-29/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+bool isAlphaNumeric(char ch) {
+    if ((ch >= '0' && ch <= '9') || (tolower(ch) >= 'a' && tolower(ch) <= 'z')) {
+        return true;
+    }
+    return false;
+}
+
+bool isPalindrome(string s) {
+
+    int st = 0, end = s.length() - 1;
+    while (st < end) {
+        if (!isAlphaNumeric(s[st])) { st++; continue; }
+        if (!isAlphaNumeric(s[end])) { end--; continue; }
+
+        if (tolower(s[st]) != tolower(s[end])) return false;
+
+        st++; end--;
+    }
+
+    return true;
+}
+
+int main() {
+    string str = "A man, a plan, a canal: Panama";
+    bool res = isPalindrome(str); 
+
+    cout << res << endl; // 1 ==> (true)
+    return 0;
+}
