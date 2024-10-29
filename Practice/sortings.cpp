7a3a3b770386e7d747d36f961c7cc30e3b4ab3fb
azimiHamid <azimihamid2003@gmail.com>
2024-10-29 10:16:15 +0500

    update

--- Practice/sortings.cpp
+++ Practice/sortings.cpp
@@ -37,12 +37,15 @@ void insertionSort(vector<int> &arr) {
     
     int n = arr.size();
 
-    for (int i = 0; i < n-1; i++) {
-        for (int j = 0; j < n-1-i; j++) {
-            if (arr[j] > arr[j+1]) {
-                swap(arr[j], arr[j+1]);
-            }
+    for (int i = 0; i < n; i++) {
+        int curr = arr[i];
+        int j = i-1;
+        while (j >= 0 && curr < arr[j]) {
+            arr[j+1] = arr[j];
+            j--;
         }
+
+        arr[j+1] = curr;
     }
 }
 
@@ -50,8 +53,8 @@ int main() {
 
     vector<int> arr = {7, 8, 3, 1, 2};
     // bubbleSort(arr);
-    selectionSort(arr);
-    // insertionSort(arr);
+    // selectionSort(arr);
+    insertionSort(arr);
 
     for (int val : arr) {
         cout << val << " ";
